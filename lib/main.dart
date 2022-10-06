255cf6287dd3a37eca154b1b347c6781742d211a
basakk123 <basakk@naver.com>
2022-10-06 09:50:12 +0900

    기본 뼈대 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:shoppingcart/components/shoppingcart_detail.dart';
+import 'package:shoppingcart/components/shoppingcart_header.dart';
+import 'package:shoppingcart/theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-// 실행 클래스
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      theme: theme(),
+      home: ShoppingCartPage(),
     );
   }
 }
 
-// 첫 페이지
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    print("HomePage 실행됨");
     return Scaffold(
+      appBar: _buildShoppingCartAppBar(),
       body: Column(
         children: [
-          DayComponent(),
-          Container(
-            color: Colors.blue,
-            height: 200,
-            child: Text("밤"),
-          ),
+          ShoppingCartHeader(),
+          ShoppingCartDetail(),
         ],
       ),
     );
   }
-}
 
-class DayComponent extends StatefulWidget {
-  const DayComponent({Key? key}) : super(key: key);
-
-  @override
-  State<DayComponent> createState() => _DayComponentState();
-}
-
-class _DayComponentState extends State<DayComponent> {
-  String upData = "낮";
-
-  @override
-  Widget build(BuildContext context) {
-    print("DayComponent 실행됨");
-    return Row(
-      children: [
-        ElevatedButton(
-            onPressed: () {
-              setState(() {
-                upData = "해";
-              });
-              print(upData);
-            },
-            child: Text("클릭")),
-        Container(
-          color: Colors.yellow,
-          height: 200,
-          child: Text("${upData}"),
-        ),
-      ],
+  AppBar _buildShoppingCartAppBar() {
+    return AppBar(
+      title: Text("ShoppingCart"),
     );
   }
 }
